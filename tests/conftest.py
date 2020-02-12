534231ab2d6ff37f3b14ea37ad17ca82ec8fa9cc
Konstantin V <mail@k-vinogradov.ru>
2020-02-12 15:08:03 +0700

    Mirror method to test added

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,14 @@ async def ping():
     return "pong"
 
 
+async def mirror(*args, **kwargs):
+    return {"args": args, "kwargs": kwargs}
+
+
 @pytest.fixture
 def endpoint():
     rpc = JsonRpcEndpoint()
     rpc.register_method(ping)
+    rpc.register_method(mirror)
+    rpc.register_method(ping, name="ping_renamed")
     return rpc
